ada1bc0fd9e35184a9999f4f2bd71f6530cda25a
2100874 <2100874@student.uwtsd.ac.uk>
2026-04-13 00:54:26 +0100

    Strawberry Bush Start
    Basic functions of the strawberry bush.
    
    Needs:
    Textures
    Tags
    Recipes
    Generation

--- src/main/java/net/theblindbandi6/lovelybites/LovelyBites.java
+++ src/main/java/net/theblindbandi6/lovelybites/LovelyBites.java
@@ -4,7 +4,9 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.theblindbandi6.lovelybites.blocks.ModBlocks;
 import net.theblindbandi6.lovelybites.items.ModItems;
+import net.theblindbandi6.lovelybites.util.ModLootTables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +29,9 @@ public class LovelyBites implements ModInitializer {
 
 		LOGGER.info("Initializing a sweet treat for myself");
 
-		ModItems.initialize();
+		ModItems.registerItems();
+		ModBlocks.registerBlocks();
+		ModLootTables.registerLootTables();
 
 		// Register the group.
 		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, LOVELY_BITES_TAB_KEY, LOVEY_BITES_TAB);
